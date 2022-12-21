22979f6993f9ea40d6d6a6a3a7284c091e4e6cf6
Matias <matiaslopez9411@gmail.com>
2022-12-21 15:54:49 -0300

    CRUD generic

--- proyectofinal/urls.py
+++ proyectofinal/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, mostrarjedis, BuscarJedi, AltaJedi, ActualizarJedi, BorrarJedi
+from .views import home, ListaJedis, BuscarJedi, NuevoJedi, JediActualizar, BorrarJedi, DetalleJedi 
 
 urlpatterns = [
     path('home/', home, name='home'),
-    path('jedis/', mostrarjedis, name='jedis-integrantes'),
+    path('panel-jedis/', ListaJedis.as_view(), name='jedis-panel'),
     path('jedis/buscar/', BuscarJedi.as_view(), name='jedis-buscar'),
-    path('jedis/alta/', AltaJedi.as_view(), name='jedis-alta'),
-    path('jedis/actualizar/<int:pk>', ActualizarJedi.as_view(), name='jedis-actualizar'),
-    path('jedis/borrar/<int:pk>', BorrarJedi.as_view(), name='jedis-borrar')
+    path('panel-jedis/alta-jedi/', NuevoJedi.as_view(), name='jedis-alta'),
+    path('panel-jedis/<int:pk>/actualizar/', JediActualizar.as_view(), name='jedis-actualizar'),
+    path('panel-jedis/<int:pk>/borrar/', BorrarJedi.as_view(), name='jedis-borrar'),
+    path('panel-jedis/<int:pk>/detalle/', DetalleJedi.as_view(), name='jedis-detalle')
 ]
--- proyectofinal/views.py
+++ proyectofinal/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from proyectofinal.models import Jedi
 from proyectofinal.forms import Buscar, JediForm
 from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, DeleteView, UpdateView
 
 #Create your views here.
 def pasar_path(request, id):
@@ -14,6 +15,27 @@ def home(request):
 def mostrarjedis(request):
     lista_jedis = Jedi.objects.all()
     return render(request, 'proyectofinal/jedis.html', {'lista_jedis': lista_jedis})
+
+class ListaJedis(ListView):
+    model = Jedi
+
+class DetalleJedi(DetailView):
+    model = Jedi
+
+class NuevoJedi(CreateView):
+    model = Jedi
+    success_url = reverse_lazy("jedis-panel")
+    fields = ['nombre','numero_jedi', 'titulo', 'color_sable']
+
+class BorrarJedi(DeleteView):
+    model = Jedi
+    success_url = reverse_lazy("jedis-panel")
+
+class JediActualizar(UpdateView):
+    template_name = 'proyectofinal/jedi_update.html'
+    model = Jedi
+    success_url = reverse_lazy("jedis-panel")
+    fields = ['nombre','numero_jedi', 'titulo', 'color_sable']
 
 class BuscarJedi(View):
     form_class = Buscar
@@ -32,7 +54,7 @@ class BuscarJedi(View):
                                                         'lista_jedis':lista_jedis})
         return render(request, self.template_name, {"form": form})
 
-class AltaJedi(View):
+""" class AltaJedi(View):
     form_class = JediForm
     template_name = 'proyectofinal/alta_jedi.html'
     initial = {'nombre':'','numero_jedi':'', 'titulo':'', 'color_sable':''}
@@ -48,9 +70,9 @@ class AltaJedi(View):
             form = self.form_class(initial=self.initial)
             return render(request, self.template_name, {'form':form, 
                                                         'msg_exito':msg_exito})
-        return render(request, self.template_name, {"form": form})
+        return render(request, self.template_name, {"form": form}) """
 
-class ActualizarJedi(View):
+"""class ActualizarJedi(View):
   form_class = JediForm
   template_name = 'proyectofinal/actualizar_jedi.html'
   initial = {'nombre':'','numero_jedi':'', 'titulo':'', 'color_sable':''}
@@ -73,13 +95,14 @@ class ActualizarJedi(View):
                                                       'jedi': jedi,
                                                       'msg_exito': msg_exito})
 
-      return render(request, self.template_name, {"form": form})
+      return render(request, self.template_name, {"form": form})"""
 
-class BorrarJedi(View):
+"""class BorrarJedi(View):
   template_name = 'proyectofinal/jedis.html'
 
   def get(self, request, pk): 
       jedi = get_object_or_404(Jedi, pk=pk)
       jedi.delete()
       lista_jedis = Jedi.objects.all()
-      return render(request, self.template_name, {'lista_jedis': lista_jedis})
+      return render(request, self.template_name, {'lista_jedis': lista_jedis})"""
+
